0115725617ad58ac5db682f6a5ad466133c2cc76
callbinuvarghese <call.binu.varghese@gmail.com>
2022-02-21 12:47:59 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: "xxxx",
+    authDomain: "angular13-firebase1.firebaseapp.com",
+    projectId: "angular13-firebase1",
+    storageBucket: "angular13-firebase1.appspot.com",
+    messagingSenderId: "151997086973",
+    appId: "xxx",
+    databaseURL:"angular13-firebase1-default-rtdb.asia-southeast1.firebasedatabase.app"
+  }
+};
